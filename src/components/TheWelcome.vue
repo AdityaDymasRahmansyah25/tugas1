c8cb5ee36a7d9cf2e62035602cc85b08da01b1f5
AdityaDymasRahmansyah25 <168413395+AdityaDymasRahmansyah25@users.noreply.github.com>
2024-06-19 03:36:27 +0700

    Tugas 1

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <h1><i>Pendidikan</i></h1>
+        SDN 002 pkl.kuras<br>
+        SMPIT at taqwa pkl.kerinci<br>
+        SMKN 1 pkl.kerinci<br>
+        Universitas islam riau
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <h1><i>Pengalaman</i></h1>
+      Pernah Magang di Kantor pertanahan nasional
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <h1><i>Keahlian</i></h1>
+      Ms. Word <br>
+      C++ <br>
+      Python <br>
+      Java <br>
+      HTML <br>
+      CSS <br>
+      JavaScript
+  </WelcomeItem>
+
+
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about">
+          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                  <h1>DATA DIRI</h1><br>
+                  <ul><i class="bi bi-chevron-right"></i> <strong>Nama                  :</strong> <span>Aditya dymas rahmansyah</span></ul>
+                  <ul><i class="bi bi-chevron-right"></i> <strong>Tempat, Tanggal lahir :</strong> <span>Pelalawan, 25 september 2004</span></ul>
+                  <ul><i class="bi bi-chevron-right"></i> <strong>Jenis Kelamin         :</strong> <span>Laki - Laki</span></ul>
+                  <ul><i class="bi bi-chevron-right"></i> <strong>Alamat                :</strong> <span>Perumahn pelita indah permai blok p no 1</span></ul>
+                  <ul><i class="bi bi-chevron-right"></i> <strong>No Telp               :</strong> <span>+62 822-8773-6849</span></ul>
+                  <ul><i class="bi bi-chevron-right"></i> <strong>email                 :</strong> <span>adityadymasrahmansyah@student.uir.ac.id</span></ul>
+                </ul>
+              </div>
+            </div>
+          </div>
+  </div> 
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+
+
+
+</style>
